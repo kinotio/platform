53b305414393c2093d9824e585d0052ac70304ba
Ando <iamando@tuta.io>
2023-08-29 18:29:13 +0300

    feature: add our community commitment section

---
+++ src/constants/commitments.ts
@@ -0,0 +1,33 @@
+import {
+  ChatBubbleLeftRightIcon,
+  TrophyIcon,
+  UserGroupIcon,
+  RocketLaunchIcon
+} from '@heroicons/react/24/solid'
+
+export const commitments = [
+  {
+    name: 'Openness and Accessibility',
+    description:
+      'We believe that together we can achieve more. Collaboration lies at the heart of everything we do. We foster an environment where individuals from diverse backgrounds and skill sets come together to collaborate on open source projects, exchange ideas, and solve complex challenges. We encourage open discussions, respectful debates, and the exchange of constructive feedback to drive the continuous improvement of our projects.',
+    icon: ChatBubbleLeftRightIcon
+  },
+  {
+    name: 'Collaboration First',
+    description:
+      'We believe that together we can achieve more. Collaboration lies at the heart of everything we do. We foster an environment where individuals from diverse backgrounds and skill sets come together to collaborate on open source projects, exchange ideas, and solve complex challenges. We encourage open discussions, respectful debates, and the exchange of constructive feedback to drive the continuous improvement of our projects.',
+    icon: TrophyIcon
+  },
+  {
+    name: 'Learning and Mentorship',
+    description:
+      "Learning is a lifelong journey, and we're here to support each other's growth. Our community is a place for both seasoned experts and aspiring learners. We offer mentorship programs, learning resources, and collaborative opportunities that foster skill development and personal growth. By nurturing an environment of knowledge sharing, we enable every member to reach their full potential.",
+    icon: UserGroupIcon
+  },
+  {
+    name: 'Giving Back to the Community',
+    description:
+      'We recognize that the open source movement thrives on giving back. As contributors, we give our time, expertise, and creativity to benefit the global developer community. Our commitment extends beyond our projects, as we actively engage in outreach, education, and support initiatives to ensure that the broader open source ecosystem flourishes.',
+    icon: RocketLaunchIcon
+  }
+]

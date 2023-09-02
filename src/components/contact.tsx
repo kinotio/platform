383ce86a3e3dfcd0b226da2b72870bcd3e314e92
Ando <iamando@tuta.io>
2023-09-02 14:40:42 +0300

    feature: setup contact component on home and remove using mocks

---
+++ src/components/contact.tsx
@@ -0,0 +1,50 @@
+import { EnvelopeIcon, MapPinIcon, PhoneIcon } from '@heroicons/react/24/outline'
+
+const Contact = () => {
+  return (
+    <section className='bg-white'>
+      <div className='container px-6 py-24 sm:py-32 mx-auto'>
+        <div className='mx-auto max-w-2xl lg:text-center'>
+          <h2 className='text-base font-semibold leading-7 text-blue-600'>Contact us</h2>
+          <p className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+            Our friendly team is always here to chat.
+          </p>
+        </div>
+
+        <div className='grid grid-cols-1 gap-12 mt-10 md:grid-cols-2 lg:grid-cols-3'>
+          <div className='flex flex-col items-center justify-center text-center'>
+            <span className='p-3 text-blue-500 rounded-full bg-blue-100/80'>
+              <EnvelopeIcon className='h-6 w-6' aria-hidden='true' />
+            </span>
+
+            <h2 className='mt-4 text-lg font-medium text-gray-800'>Email</h2>
+            <p className='mt-2 text-gray-500'>Our friendly team is here to help.</p>
+            <p className='mt-2 text-blue-500'>hello@merakiui.com</p>
+          </div>
+
+          <div className='flex flex-col items-center justify-center text-center'>
+            <span className='p-3 text-blue-500 rounded-full bg-blue-100/80'>
+              <MapPinIcon className='h-6 w-6' aria-hidden='true' />
+            </span>
+
+            <h2 className='mt-4 text-lg font-medium text-gray-800'>Office</h2>
+            <p className='mt-2 text-gray-500'>Come say hello at our office HQ.</p>
+            <p className='mt-2 text-blue-500'>100 Smith Street Collingwood VIC 3066 AU</p>
+          </div>
+
+          <div className='flex flex-col items-center justify-center text-center'>
+            <span className='p-3 text-blue-500 rounded-full bg-blue-100/80'>
+              <PhoneIcon className='h-6 w-6' aria-hidden='true' />
+            </span>
+
+            <h2 className='mt-4 text-lg font-medium text-gray-800'>Phone</h2>
+            <p className='mt-2 text-gray-500'>Mon-Fri from 8am to 5pm.</p>
+            <p className='mt-2 text-blue-500'>+1 (555) 000-0000</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,64 +1,19 @@
-import { community, help, legal } from '@/mocks/footer'
-import { social } from '@/mocks/social'
+import FooterSocialMediaComponent from '@/components/footer/social'
+import FooterLegalComponent from './footer/legal'
+import FooterHelpCenterComponent from './footer/help'
+import FooterCommunityComponent from './footer/community'
 
 const Footer = () => {
   return (
     <footer className='kn-bg-spaces py-32 px-12'>
       <div className='mx-auto w-full max-w-screen-xl'>
         <div className='grid grid-cols-1 gap-8 px-4 py-6 lg:grid-cols-3 lg:py-8 md:grid-cols-3'>
-          <div>
-            <h2 className='mb-6 text-md font-semibold text-white'>Community</h2>
-            <ul className='text-white font-medium'>
-              {community.map(({ name, link }: { name: string; link: string }) => (
-                <li key={name} className='text-sm mb-4'>
-                  <a href={link} className=' hover:underline'>
-                    {name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div>
-            <h2 className='mb-6 text-md font-semibold text-white'>Help center</h2>
-            <ul className='text-white font-medium'>
-              {help.map(({ name, link }: { name: string; link: string }) => (
-                <li key={name} className='text-sm mb-4'>
-                  <a href={link} className=' hover:underline'>
-                    {name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div>
-            <h2 className='mb-6 text-md font-semibold text-white'>Legal</h2>
-            <ul className='text-white font-medium'>
-              {legal.map(({ name, link }: { name: string; link: string }) => (
-                <li key={name} className='text-sm mb-4'>
-                  <a href={link} className=' hover:underline'>
-                    {name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
+          <FooterCommunityComponent />
+          <FooterHelpCenterComponent />
+          <FooterLegalComponent />
         </div>
 
-        <div className='px-4 py-6 md:flex md:items-center md:justify-between'>
-          <span className='text-sm text-white sm:text-center'>
-            © 2023 <a href='https://github.com/kinotio'>Kinotio</a>. All Rights Reserved.
-          </span>
-          <div className='flex mt-4 space-x-5 sm:justify-center md:mt-0'>
-            {social.map(({ name, icon: Icon, link }: { name: string; icon: any; link: string }) => (
-              <a key={name} href={link} className='text-sm text-white'>
-                <Icon />
-                <span className='sr-only'>{name}</span>
-              </a>
-            ))}
-          </div>
-        </div>
+        <FooterSocialMediaComponent />
       </div>
     </footer>
   )

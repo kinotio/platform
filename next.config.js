8a9fc18f6f369bdedb4d404195621baf07adcbbe
Ando <iamando@tuta.io>
2023-09-02 16:34:53 +0300

    feature: update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
-  output: 'export'
+  output: 'export',
+  basePath: '/github-pages',
+  reactStrictMode: true
 }
 
 module.exports = nextConfig

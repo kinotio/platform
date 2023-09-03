e2acefcaa4d50a0f7991115414f432fd546f01de
Ando <iamando@tuta.io>
2023-09-03 15:40:55 +0300

    feature: remove .env for now to deploy on vercel

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const isDevelopment = process.env.NEXT_PUBLIC_APP_ENV === 'development'
-
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
 
-  output: 'export',
-
-  basePath: isDevelopment ? '' : '/platform',
-  assetPrefix: isDevelopment ? '' : '/platform/'
+  output: 'export'
 }
 
 module.exports = nextConfig

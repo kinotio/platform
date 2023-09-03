ccf75b2e0ed8f2504c98f94181939255eaeef3b6
Ando <iamando@tuta.io>
2023-09-03 15:14:24 +0300

    feature: create env local and deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isDevelopment = process.env.NEXT_PUBLIC_APP_ENV === 'development'
+
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+
   output: 'export',
-  basePath: '/',
-  images: {
-    loader: 'akamai',
-    path: '/'
-  }
+
+  basePath: isDevelopment ? '' : '/platform',
+  assetPrefix: isDevelopment ? '' : '/platform/'
 }
 
 module.exports = nextConfig

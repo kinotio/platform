e018b981dffcde39c5547fa8ccc52284f1b0a608
Ando <iamando@tuta.io>
2023-09-10 12:36:17 +0300

    feature: refont platform delete all components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import HeroSectionComponent from '@/components/pages/home/hero/section'
-import CommitmentSectionCompenent from '@/components/pages/home/commitment/section'
-import SubscribeFormComponent from '@/components/pages/home/subscribe/section'
-
 const Page = () => {
   return (
     <main className='bg-blue-950'>
-      <div className='kn-header-and-hero-container'>
-        <HeroSectionComponent />
-      </div>
-      <CommitmentSectionCompenent />
-      <SubscribeFormComponent />
+      <h1>Hello, world</h1>
     </main>
   )
 }

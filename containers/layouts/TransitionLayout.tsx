d8c209ef2f130a7e40d8f885144d88309f6fb402
Ando <iamando@tuta.io>
2023-09-15 19:36:49 +0300

    feature: setup cookie container and timeout the showing process

--- containers/layouts/TransitionLayout.tsx
+++ containers/layouts/TransitionLayout.tsx
@@ -1,27 +1,17 @@
 'use client'
 
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 import { v4 as uuidv4 } from 'uuid'
 
 import HeaderComponent from '@components/common/HeaderComponent'
 import FooterComponent from '@components/common/FooterComponent'
-import CookieComponent from '@components/common/CookieComponent'
 
 const TransitionLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <AnimatePresence>
+    <AnimatePresence initial={false}>
       <HeaderComponent key={uuidv4()} />
       <main className='container mx-auto'>{children}</main>
       <FooterComponent key={uuidv4()} />
-
-      <motion.div
-        initial={{ y: '120vh' }}
-        animate={{ y: 0 }}
-        transition={{ ease: 'easeInOut', delay: 0.1 }}
-        key={uuidv4()}
-      >
-        <CookieComponent />
-      </motion.div>
     </AnimatePresence>
   )
 }

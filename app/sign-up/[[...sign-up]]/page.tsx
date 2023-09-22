ebe0103502e296e44618092420841ef6bd16f8b3
Ando <iamando@tuta.io>
2023-09-22 13:48:02 +0300

    feature: redirect to home after auth

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ const Page = () => {
   return (
     <main className='container mx-auto flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='mx-auto'>
-        <SignUp />
+        <SignUp afterSignUpUrl={'/'} />
       </div>
     </main>
   )
